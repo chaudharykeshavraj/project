8c161a0efad20530c185463c71ed83f3d9329ee9
chaudharykeshavraj <keshavrajc31@gmail.com>
2025-06-29 22:29:28 +0545

    project 6

---
+++ ui/src/library/http.js
@@ -0,0 +1,21 @@
+import axios from "axios"
+
+const http = axios.create({
+    baseURL: 'http://localhost:5000',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
+})
+
+http.interceptors.request.use(config => {
+    const token = localStorage.getItem('mbttoken')  // data chha bhane token dinchha otherwise null
+
+    if(token) {
+        config.headers.setAuthorization(`Bearer ${token}`)
+    }
+
+    return config
+})
+
+export default http
--- ui/src/pages/cms/auth/Login.jsx
+++ ui/src/pages/cms/auth/Login.jsx
@@ -1,11 +1,47 @@
-import { Button, Col, Form, Row } from "react-bootstrap"
+import { Button, Col, Form, Row, Spinner } from "react-bootstrap"
+import { useFormik } from "formik"
+import * as Yup from "yup"
+import http from "../../../library/http"
 
 export const Login = () => {
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().required('Milena bhai milena').email(),
+            password: Yup.string().required('Password milena tero'),
+        }),
+        onSubmit: (data, {setSubmitting}) => {    //form ko data haru (data) maa aauchha
+            http.post('/auth/login', data)
+                .then(({data}) => {
+                    //console.log
+                    localStorage.setItem('mbcttoken', data.token)
+
+                    return http.get('/profile/details')  /*, {
+                        headers: {
+                            Authorization: `Bearer ${data.token}`
+                        }
+                    }) */
+                })
+                .then(({data}) => {
+                    console.log(data)
+                })
+                .catch(({response}) => {
+                    //console.log(error)
+                    if('validation' in response.data) {
+                        formik.setErrors(response.data.validation)
+                    }
+                })
+                .finally(() => setSubmitting(false))
+        }
+    })
     return <>
         <Row className="vh-100 align-items-center justify-content-center">
             <Col lg="4" className="bg-white my-5 py-3 rounded-2 shadow-sm mx-auto">
                 <h1 className="text-center">Log In</h1>
-                <Form>
+                <Form onSubmit={formik.handleSubmit}>
                     <Form.Group className="mb-3">
                         <Form.Label htmlFor="email">Email</Form.Label>
 
@@ -14,7 +50,17 @@ export const Login = () => {
                             id="email"
                             type="email"
                             placeholder="Enter email"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.email}
+                            isInvalid={formik.touched.email && formik.errors.email}
+                            isValid={formik.values.email && !formik.errors.email}
                         />
+
+                        {formik.touched.email && formik.errors.email && <Form.Control.Feedback type="invalid">
+                            {formik.errors.email}
+                        </Form.Control.Feedback>}
+
                     </Form.Group>
 
                     <Form.Group className="mb-3">
@@ -25,11 +71,23 @@ export const Login = () => {
                             id="password"
                             type="password"
                             placeholder="Enter password"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            isInvalid={formik.touched.password && formik.errors.password}
+                            isValid={formik.values.password && !formik.errors.password}
                         />
+
+                        {formik.touched.password && formik.errors.password && <Form.Control.Feedback type="invalid">
+                            {formik.errors.password}
+                        </Form.Control.Feedback>}
                     </Form.Group>
 
-                    <Button variant="dark" type="submit">
-                        <i className="bi bi-box-arrow-in-right me-2"></i>Log In
+                    <Button variant="dark" type="submit" disabled={formik.isSubmitting}>
+                        {formik.isSubmitting ? <>
+                            <Spinner />Processing...
+                        </> : <>
+                            <i className="bi bi-box-arrow-in-right me-2"></i>Log In
+                        </>}
                     </Button>
                 </Form>
             </Col>
